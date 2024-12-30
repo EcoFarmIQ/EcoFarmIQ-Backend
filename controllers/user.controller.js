9b1af7499ea056f249dc310c0040167dedd4a48e
EcoFarmIQ <hbhayle@gmail.com>
2024-12-30 17:25:54 +0300

    --date=2024-12-4 14:30:00

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -2,8 +2,7 @@ const User = require("../models/User");
 const jwt = require("jsonwebtoken");
 const bcrypt = require("bcrypt");
 const nodemailer = require("nodemailer");
- 
-   
+
 // Register new user
 exports.registerUser = async (req, res) => {
   const { name, email, password } = req.body;
